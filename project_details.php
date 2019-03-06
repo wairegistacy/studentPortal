e7b3a1c333d46fff5680ea340932fa9fdfb23f74
StacyWairegi <48006331+wairegistacy@users.noreply.github.com>
2019-03-06 15:44:20 +0300

    Add files via upload

--- project_details.php
+++ project_details.php
@@ -1,3 +1,11 @@
+<?php
+/**
+* Created by PhpStorm.
+* User: Stacy Wairegi
+* Date: 06/03/2019
+* Time: 14:29
+*/
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,58 +24,84 @@
 	<!--[if lt IE 9]>
 <!--	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>-->
 	<!--	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
+	<style>
+		h1{
+			color: blue;
+			text-align: center;
+			border-bottom: black;
+		}
+
+	</style>
 
 </head>
 <body>
+<?php if(isset($_SESSION['success'])) { ?>
+
+	<div class="alert alert-danger"> <?php echo $_SESSION['success']; ?></div>
+	<?php
+}
+?>
+<?php echo validation_errors('<div class="alert alert-danger">','</div>');?>
 <div class="col-lg-8 col-lg-offset-2">
 	<h1>PROJECT DETAILS: </h1>
 	<p>Please fill in the following details: </p>
 
-	<?php if(isset($_SESSION['success'])) { ?>
-
-		<div class="alert alert-danger"> <?php echo $_SESSION['success']; ?></div>
-		<?php
-	}
-	?>
-	<?php echo validation_errors('<div class="alert alert-danger">','</div>');?>
-
 	<form action=" " method="post">
 
 		<div class="form-group">
-			<label for="Project_title" >Project Title: </label>
-			<input class="form-control" required name="Project_title" id="Project_title" type="text">
+			<label for="category" >Category of the Project: </label><br>
+			<select class="form-control" id="county" required name="county">
+				<option value=" "> </option>
+				<option value="Chemical, physical and Mathematical sciences">Chemical, physical and Mathematical sciences</option>
+				<option value="Social and Behavioral sciences">Social and Behavioral sciences</option>
+				<option value="Biological and Ecological Sciences">Biological and Ecological Sciences</option>
+				<option value="Technology">Technology</option>
+			</select>
 		</div>
 		<div class="form-group">
-			<label for="Name_of_school" >Name of school: </label>
-			<input class="form-control" required name="Name_of_school" id="Name_of_school" type="text">
+			<label for="level" >Level: </label><br>
+			<select class="form-control" id="level" required name="level">
+				<option value=" "> </option>
+				<option value="Junior(Form 1 and Form 2)">Junior(Form 1 and Form 2)</option>
+				<option value="Senior (Form 3 and Form 4)">Senior (Form 3 and Form 4)</option>
+			</select>
+		</div>
+		<div class="form-group">
+			<label for="Project_title" >Project Title: </label><br>
+			<input class="form-control" required name="Project_title" id="Project_title" type="text"><br>
+		</div>
+		<div class="form-group">
+			<label for="Name_of_school" >Name of school: </label><br>
+			<input class="form-control" required name="Name_of_school" id="Name_of_school" type="text"><br>
 		</div>
 		<div class="form-group">
 			<label for="Student_name" >Student Names: </label>
 			<input class="form-control" required name="Student_name" id="Student_name" type="text">
+			<input class="form-control" required name="Student_name" id="Student_name" type="text">
 		</div>
 			<div class="form-group">
 				<label for="Introduction" >Introduction: </label>
-				<textarea required name="Introduction" rows="40" cols="160" placeholder="Write your Introduction here"></textarea>
+				<textarea required name="Introduction" rows="20" cols="150" placeholder="Write your Introduction here"></textarea>
 			</div>
 			<div class="form-group">
 				<label for="Method" >Method: </label>
-				<textarea required name="Method" rows="100" cols="160" placeholder="Write your method here"></textarea>
+				<textarea required name="Method" rows="20" cols="150" placeholder="Write your method here"></textarea>
 			</div>
 		<div class="form-group">
 			<label for="Results" >Results: </label>
-			<textarea required name="Results" rows="100" cols="160" placeholder="Write your results here"></textarea>
+			<textarea required name="Results" rows="20" cols="150" placeholder="Write your results here"></textarea>
 		</div>
 		<div class="form-group">
 			<label for="Conclusion" >Conclusion: </label>
-			<textarea required name="Conclusion" rows="40" cols="160" placeholder="Write your conclusion here"></textarea>
+			<textarea required name="Conclusion" rows="20" cols="150" placeholder="Write your conclusion here"></textarea>
 		</div>
 		<div class="form-group">
 			<label for="Referees" >References: </label>
-			<textarea required name="Referees" rows="40" cols="160" placeholder="Write your references here"></textarea>
+			<textarea required name="Referees" rows="20" cols="150" placeholder="Write your references here"></textarea>
 		</div>
 		<div class="form-group">
 			<label for="Acknowledge" >Acknowledgments: </label>
-			<textarea required name="Acknowledge" rows="40" cols="160" placeholder="Write your acknowledgments here"></textarea>
+			<textarea required name="Acknowledge" rows="20" cols="150" placeholder="Write your acknowledgments here"></textarea>
 		</div>
 			<div class="text-center">
 				<button class="btn btn-primary" name="project_details">Save</button>
@@ -76,81 +110,6 @@
 
 	</form>
 
-<!--	<script>-->
-<!---->
-<!--		function checkPassword(){-->
-<!---->
-<!--			if (document.getElementById('password').value !== document.getElementById('cPassword').value){-->
-<!--				alert("Passwords do not match!");-->
-<!--				document.getElementById('password').value = "";-->
-<!--				document.getElementById('cPassword').value = "";-->
-<!---->
-<!--				return false;-->
-<!---->
-<!--			}-->
-<!--		}-->
-<!---->
-<!--		function CheckPasswordStrength(password) {-->
-<!--			var password_strength = document.getElementById("password_strength");-->
-<!---->
-<!---->
-<!--			if (password.length == 0) {-->
-<!--				password_strength.innerHTML = "";-->
-<!--				return;-->
-<!--			}-->
-<!---->
-<!---->
-<!--			var regex = new Array();-->
-<!--			regex.push("[A-Z]");-->
-<!--			regex.push("[a-z]");-->
-<!--			regex.push("[0-9]");-->
-<!--			regex.push("[$@$!%*#?&]");-->
-<!---->
-<!--			var passed = 0;-->
-<!---->
-<!---->
-<!--			for (var i = 0; i < regex.length; i++) {-->
-<!--				if (new RegExp(regex[i]).test(password)) {-->
-<!--					passed++;-->
-<!--				}-->
-<!--			}-->
-<!---->
-<!---->
-<!--			if (passed > 2 && password.length > 8) {-->
-<!--				passed++;-->
-<!--			}-->
-<!---->
-<!---->
-<!--			var color = "";-->
-<!--			var strength = "";-->
-<!--			switch (passed) {-->
-<!--				case 0:-->
-<!--				case 1:-->
-<!--					strength = "Weak";-->
-<!--					color = "orange";-->
-<!--					break;-->
-<!--				case 2:-->
-<!--					strength = "Good";-->
-<!--					color = "darkorange";-->
-<!--					break;-->
-<!--				case 3:-->
-<!--				case 4:-->
-<!--					strength = "Strong";-->
-<!--					color = "yellow";-->
-<!--					break;-->
-<!--				case 5:-->
-<!--					strength = "Very Strong";-->
-<!--					color = "red";-->
-<!--					break;-->
-<!--			}-->
-<!--			password_strength.innerHTML = strength;-->
-<!--			password_strength.style.color = color;-->
-<!--		}-->
-<!--	</script>-->
 
-<!--<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
-<!--<!-- Include all compiled plugins (below), or include individual files as needed -->
-<!--<script src="--><?php //echo base_url();?><!--assets/js/bootstrap.min.js"></script>-->
 </body>
 </html>
